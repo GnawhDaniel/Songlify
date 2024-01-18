211f7db41918c62e8142937f36d53b0ed1b888b5
Daniel Hwang <danielhwang02@gmail.com>
2024-01-17 23:58:50 -0600

    Created search feature.

--- Musicle/src/App.tsx
+++ Musicle/src/App.tsx
@@ -1,29 +1,30 @@
 import { useState, useEffect } from 'react';
-import WebPlayback from './components/WebPlayback.tsx'
-import Login from './components/Login'
+import 'bootstrap/dist/css/bootstrap.css';
+import Search from "@components/Search"
 import './App.css';
 
 function App() {
 
-  const [token, setToken] = useState('');
+  // const [token, setToken] = useState('');
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    async function getToken() {
-      const response = await fetch('http://localhost:5000/auth/token');
-      const json = await response.json();
-      console.log(json);
-      setToken(json.access_token);
-    }
+  //   async function getToken() {
+  //     const response = await fetch('http://localhost:5000/auth/token');
+  //     const json = await response.json();
+  //     console.log(json);
+  //     setToken(json.access_token);
+  //   }
 
-    getToken();
+  //   getToken();
 
-  }, []);
+  // }, []);
 
   return (
     <>
-        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
-        {/* { (token === '') ? <Login/> : <h1>Token</h1> } */}
+        <h1>SonGuess</h1>
+        <Search/>
+        {/* <WebPlayback token={token} /> */}
     </>
   );
 }
