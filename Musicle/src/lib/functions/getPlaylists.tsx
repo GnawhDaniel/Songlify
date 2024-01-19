69aa4a27b259b8b89c85d747399604460426b6be
Daniel Hwang <danielhwang02@gmail.com>
2024-01-18 21:50:01 -0600

    function improvements

--- Musicle/src/lib/functions/getPlaylists.tsx
+++ Musicle/src/lib/functions/getPlaylists.tsx
@@ -1,21 +1,33 @@
 export async function getPlaylists(query: string) {
+    if (query === "") {
+        return []
+    }
+
     if (query.includes("https://open.spotify.com/playlist")){
         // return exact match
     }
-    
+
     let encodedQuery = encodeURIComponent(query);
     const endpoint = import.meta.env.VITE_SERVER;
     const spotify = `${endpoint}/auth/getPlaylists?name=${encodedQuery}`
-
-    console.log(spotify);
+    let playlists = []
 
     try {
         const response = await fetch(spotify);
-        const data = await response.json();
-        console.log(data);
-        // Process the data
+        let data = await response.json();
+        data = data['playlists']['items']
+        for (let i = 0; i < data.length; i++) {
+            playlists.push({
+                name: data[i]["name"],
+                image: data[i]["images"][0]["url"],
+                owner: data[i]["owner"]["display_name"],
+                uri: data[i]["uri"]
+            })
+        }
     } catch (error) {
         console.error('Error:', error);
     }
+
+    return playlists
 };
 
