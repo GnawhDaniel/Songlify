8f7e93a042536a980b4c3496ec15751d1f3119e1
Daniel Hwang <danielhwang02@gmail.com>
2024-01-18 21:30:08 -0600

    init

--- Musicle/src/lib/components/List/List.tsx
+++ Musicle/src/lib/components/List/List.tsx
@@ -1,5 +1,31 @@
-function List() {
+import {Playlist} from "@lib/interfaces";
+import "./List.css"
 
+interface Iprops {
+    val: Playlist[]
+}
+
+function List(props: Iprops) {
+    let items = props.val
+    console.log(items)
+
+    return (
+        <>
+            <div className="playlist overflow-auto ">
+                {items.map((item, index) => (
+                    <div className="row " key={index}>
+                        <a href="">
+                            <img src={item.image} alt="" />
+                            <div className="inner-row">
+                                <div className="name fs-5">{item.name}</div>
+                                <div className="owner fs-7">{item.owner}</div>  
+                            </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default List;
