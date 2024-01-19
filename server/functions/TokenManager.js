f15c8f6a38578cae3b9cfb360609b0e3c864f0b5
Daniel Hwang <danielhwang02@gmail.com>
2024-01-18 21:07:17 -0600

    Class object for token + class functions.

---
+++ server/functions/TokenManager.js
@@ -0,0 +1,63 @@
+const request = require('request');
+
+
+class TokenManager {
+    constructor(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET) {
+        this.accessToken = '';
+        this.SPOTIFY_CLIENT_ID = SPOTIFY_CLIENT_ID;
+        this.SPOTIFY_CLIENT_SECRET = SPOTIFY_CLIENT_SECRET;
+        this.expirationTime = null;
+    }
+
+    setAccessToken(token) {
+        this.expirationTime = Date.now() + (1 * 60 * 60 * 1000)
+        this.accessToken = token;
+    }
+
+    getAccessToken() {
+        return this.accessToken;
+    }
+
+    isTokenExpired() {
+        return !this.accessToken || !this.expirationTime || Date.now() >= this.expirationTime
+    }
+
+    // Function to retrieve a new access token from the Spotify API
+    retrieveNewToken() {
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
+            },
+            form: {
+                grant_type: 'client_credentials'
+            },
+            json: true
+        };
+
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                this.setAccessToken(body.access_token);
+                console.log("Access token updated");
+            } else {
+                // Handle errors or unsuccessful token retrieval
+                console.error("Failed to retrieve token", error);
+            }
+        });
+    }
+
+    // Function to check and refresh token if necessary
+    refreshTokenIfNeeded() {
+        // Logic to determine if the token needs to be refreshed
+        // This could be based on time, error response, etc.
+        // For example:
+        // if (this.accessToken is expired) {
+        //     this.retrieveNewToken();
+        // }
+
+        // For simplicity, this example just retrieves a new token
+        this.retrieveNewToken();
+    }
+}
+
+module.exports = TokenManager;
