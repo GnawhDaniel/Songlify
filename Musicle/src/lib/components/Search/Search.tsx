8a42fdfebcdbb8c43a32b851ce6c774f650accce
Daniel Hwang <danielhwang02@gmail.com>
2024-01-18 21:49:42 -0600

    css changes

--- Musicle/src/lib/components/Search/Search.tsx
+++ Musicle/src/lib/components/Search/Search.tsx
@@ -1,32 +1,46 @@
-import "@functions/getPlaylists"
-import "./Search.css"
+import "@functions/getPlaylists";
+import List from "@components/List";
+import "./Search.css";
 import { getPlaylists } from "@functions/getPlaylists";
-import React, { useState, useEffect} from 'react';
-
+import { useState, useEffect } from "react";
+import { Playlist } from '@/lib/interfaces'; // Import the interface
 
 
 function Search() {
-    const [searchQuery, setSearchQuery] = useState('');
-
+    const [searchQuery, setSearchQuery] = useState("");
+    const [playlists, setPlaylists] = useState<Playlist[]>([]);
 
     // Function to handle input change
     const handleInputChange = (event: any) => {
         setSearchQuery(event.target.value);
     };
 
+    async function handleGetPlaylists() {
+        const response = await getPlaylists(searchQuery);
+        setPlaylists(response);
+    }
+
     useEffect(() => {
         const timer = setTimeout(() => {
-            getPlaylists(searchQuery)
-        }, 1000)
-    
-        return () => clearTimeout(timer)
-      }, [searchQuery])
+            handleGetPlaylists();
+        }, 1000);
 
+        return () => clearTimeout(timer);
+    }, [searchQuery]);
 
     return (
-        <div className="Search">
-            <input onChange={handleInputChange} className="form-control mr-sm-2 input-large" type="search" placeholder="Search" aria-label="Search"/>
-        </div>
+        <>
+            <div className="Search">
+                <input
+                    onChange={handleInputChange}
+                    className="form-control mr-sm-2 input-large"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+            </div>
+            <List val={playlists}/>
+        </>
     );
 }
 
