1ead2e7f8559140df705b233ff1b538e899097b5
Daniel Hwang <danielhwang02@gmail.com>
2024-01-19 01:35:24 -0600

    added routes.

--- Musicle/src/App.tsx
+++ Musicle/src/App.tsx
@@ -1,14 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Search from "@components/Search"
+import SongsPage from '@components/SongsPage';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-        <h1>SonGuess</h1>
-        <Search/>
-        {/* <WebPlayback token={token} /> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Search/>}></Route>
+            <Route path="/playlist/:id" element={<SongsPage/>}></Route>
+          </Routes>
+        </Router>
     </>
   );
 }
--- Musicle/src/lib/components/List/List.tsx
+++ Musicle/src/lib/components/List/List.tsx
@@ -1,15 +1,18 @@
-import {Playlist} from "@lib/interfaces";
-import "./List.css"
+import { Playlist } from "@lib/interfaces";
+import "./List.css";
+import { useNavigate, Link } from 'react-router-dom'
 
 interface Iprops {
-    val: Playlist[]
+    val: Playlist[];
 }
 
 function List(props: Iprops) {
-    let items = props.val
-    console.log(items)
+    const navigate = useNavigate();
+
+    let items = props.val;
+    console.log(items);
     if (items.length === 0) {
-        return (<div>Type in a Spotify playlist or link</div>)
+        return <div>Type in a Spotify playlist or link</div>;
     }
 
     return (
@@ -17,13 +20,13 @@ function List(props: Iprops) {
             <div className="playlist overflow-auto ">
                 {items.map((item, index) => (
                     <div className="row " key={index}>
-                        <a href="">
+                        <Link to={`/playlist/${item.name}`} state={{"playlist": item}}>
                             <img src={item.image} alt="" />
                             <div className="inner-row">
                                 <div className="name fs-5">{item.name}</div>
-                                <div className="owner fs-7">{item.owner}</div>  
+                                <div className="owner fs-7">{item.owner}</div>
                             </div>
-                        </a>
+                        </Link>
                     </div>
                 ))}
             </div>
