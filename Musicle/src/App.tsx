7a362079c3a76cf901c12172fa411606c00c8524
Daniel Hwang <danielhwang02@gmail.com>
2024-01-18 21:49:27 -0600

    css change.

--- Musicle/src/App.tsx
+++ Musicle/src/App.tsx
@@ -1,24 +1,8 @@
-import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Search from "@components/Search"
 import './App.css';
 
 function App() {
-
-  // const [token, setToken] = useState('');
-
-  // useEffect(() => {
-
-  //   async function getToken() {
-  //     const response = await fetch('http://localhost:5000/auth/token');
-  //     const json = await response.json();
-  //     console.log(json);
-  //     setToken(json.access_token);
-  //   }
-
-  //   getToken();
-
-  // }, []);
 
   return (
     <>
