94fceaf36511bf3be481ffd1a37c3c4b587797ac
Daniel Hwang <danielhwang02@gmail.com>
2024-01-20 02:20:33 -0600

    major feature implementations.

--- Musicle/src/lib/components/SongsPage/SongsPage.tsx
+++ Musicle/src/lib/components/SongsPage/SongsPage.tsx
@@ -3,12 +3,14 @@ import { getSongs } from "@/lib/functions/getSongs";
 import React, { useState, useEffect } from "react";
 import PlayButton from "@/assets/play-button.svg";
 import SkipButton from "@/assets/skip-icon.svg";
+import PauseButton from "@/assets/pause-button.svg";
 import { useLocation } from "react-router-dom";
 import { Songs } from "@/lib/interfaces";
 import { Link } from "react-router-dom";
 import "./SongsPages.css";
 
 import { returnQuery } from "@/lib/functions/returnQuery";
+import { getEmbed } from "@/lib/functions/getEmbed";
 
 function SongsPage() {
     // TO DO:
@@ -18,6 +20,7 @@ function SongsPage() {
     // let { id } = useParams();
     const location = useLocation();
     const playlist = location.state.playlist;
+    const randomNumber = location.state.randomNumber;
 
     const [guessVal, setGuessVal] = useState<string>("");
     const [songs, setSongs] = useState<Songs[]>([]);
@@ -30,8 +33,15 @@ function SongsPage() {
         "",
     ]);
 
-    const times = [1, 2, 4, 7, 11, 16, 21]
+    const times = [1, 2, 4, 7, 11, 16, 21];
     const [counter, setCounter] = useState<number>(0);
+    const [randomSong, setRandomSong] = useState<Songs>();
+    const [audio, setAudio] = useState<HTMLAudioElement>();
+    const [paused, setPaused] = useState(true);
+    const [win, setWin] = useState(false);
+    const [endGame, setEndGame] = useState(false);
+    // const [embed, setEmbed] = useState<string>("<div></div>");
+
     const allowedAttempts = 6;
 
     // Fetch the songs when the component mounts or playlist.id changes
@@ -48,48 +58,90 @@ function SongsPage() {
         fetchSongs();
     }, [playlist.id]);
 
+    // Pick Random Song from Songs Array
+    useEffect(() => {
+        if (songs.length > 0) {
+            const selectedSong = songs[Math.floor(randomNumber * songs.length)];
+            setRandomSong(selectedSong);
+            setAudio(new Audio(selectedSong["preview"]));
+        }
+    }, [songs]);
+    
+    // // Get Embed
+    // useEffect(() => {
+    //     const getEmbedded = async () => {
+    //         try {
+    //             if (randomSong && randomSong["url"]) {
+    //                 console.log(randomSong["url"]);
+    //                 const fetchedEmbed = await getEmbed(randomSong["url"]);
+    //                 console.log(fetchedEmbed);
 
-    // function returnQuery() {
-    //     if (guessVal === "") {
-    //         return [];
-    //     }
-    //     var valid = [];
-    //     for (let i = 0; i < songs.length; i++) {
-    //         var track = `${songs[i]["artist"]} - ${songs[i]["songName"]}`;
-    //         var trackLower = track.toLowerCase();
-    //         if (trackLower.includes(guessVal.toLowerCase())) {
-    //             valid.push(track);
+    //                 setEmbed(fetchedEmbed)
+    //                 console.log("here")
+    //                 console.log(embed)
+    //             }
+    //         } catch (error) {
+    //             console.error("Error fetching embed:", error);
     //         }
-    //     }
-    //     return valid;
-    // }
+    //     };
 
+    //     getEmbedded();
+    // }, [ randomSong?.["url"] ]);
+    
+    
+    // Handler Functions
     function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
         setGuessVal(event.target.value);
         returnQuery(songs, guessVal);
     }
 
-    function handleLoss() {}
-
-    // function handleWin() {}
-
     function handleSelect(song: string) {
         setGuessVal(song);
     }
 
+    function handlePlay() {
+        if (audio && audio.paused) {
+            audio.currentTime = 0;
+            audio.volume = 0.45;
+            audio.play();
+            setPaused(false);
+            setTimeout(() => {
+                audio.pause();
+                setPaused(true);
+            }, times[counter] * 1000);
+        } else {
+        }
+    }
+
+    function handlePause() {
+        if (audio && !audio.paused) {
+            audio.pause();
+            setPaused(true);
+        }
+    }
+
+    function handleSkip() {}
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
+        if (endGame) {
+            return
+        }
         // add if statement for win or loss
-
         for (let i = 0; i < songs.length; i++) {
-            if (
-                `${songs[i]["artist"]} - ${songs[i]["songName"]}}`.includes(
-                    guessVal
-                )
-            ) {
-                if (counter >= allowedAttempts) {
-                    handleLoss();
+            var currSong = `${songs[i]["artist"]} - ${songs[i]["songName"]}`;
+            if (currSong.includes(guessVal) && currSong === guessVal) {
+                if (
+                    randomSong &&
+                    guessVal ===
+                        `${randomSong["artist"]} - ${randomSong["songName"]}`
+                ) {
+                    setWin(true);
+                    setEndGame(true);
+                }
+                if (counter === allowedAttempts - 1) {
+                    setEndGame(true);
                 }
 
                 var arr = guessArray;
@@ -111,7 +163,7 @@ function SongsPage() {
 
             <div className="songs-page">
                 <div className="playlist-info">
-                    <img src={playlist.image} alt="spotiy-playlist-image" />
+                    <img src={playlist.image} alt="spotify-playlist-image" />
                     <h2>{playlist.name}</h2>
                 </div>
 
@@ -163,12 +215,35 @@ function SongsPage() {
                 <div className="progess-bar"></div>
 
                 <div className="actions">
-                    <img className="skip" onClick="" draggable="false" src={SkipButton} alt="" />
-                    <img className="play-pause" onClick="" draggable="false" src={PlayButton} alt="" />
+                    <img
+                        className="skip"
+                        onClick={() => handleSkip()}
+                        draggable="false"
+                        src={SkipButton}
+                        alt=""
+                    />
+                    <img
+                        className="play-pause"
+                        onClick={() => {paused ? handlePlay() : handlePause()}}
+                        draggable="false"
+                        src={paused ? PlayButton : PauseButton}
+                        alt=""
+                    />
                 </div>
-
-
             </div>
+            {endGame && randomSong && (
+                <div className="overlay">
+                    <div className="popup">
+                        <img src={randomSong["image"]} alt="" />
+                        <div className="end-text">
+                            <b>{win ? "Winner!" : "Better Luck Next Time."}</b>
+                            <i>{randomSong["songName"]} by {randomSong["artist"]}</i>
+                        </div>
+                        {/* <div dangerouslySetInnerHTML={{__html: embed}}/> */}
+                    </div>
+                </div>
+                
+            )}
         </>
     );
 }
