fef6ae612cebe931e0410a1de4cacee928d9a856
Daniel Hwang <danielhwang02@gmail.com>
2024-01-20 02:21:41 -0600

    added necessary fields to getSongs.

--- Musicle/src/lib/functions/getSongs.tsx
+++ Musicle/src/lib/functions/getSongs.tsx
@@ -17,20 +17,22 @@ export async function getSongs(query: string) {
         let data = await response.json();
         // console.log("data", data["items"][0]["track"]["artists"][0]["name"])
         data = data["items"]
+        // console.log(data)
         for (let i = 0; i < data.length; i++) {
             if (data[i]["track"] != null && data[i]["track"]["preview_url"] != null) {
                 tracks.push({
                     artist: data[i]["track"]["artists"][0]["name"],
                     songName: data[i]["track"]["name"],
-                    preview: data[i]["track"]["preview_url"]
+                    preview: data[i]["track"]["preview_url"],
+                    image: data[i]["track"]["album"]["images"][0]["url"],
+                    url: data[i]["track"]["external_urls"]["spotify"]
                 })
             }
-      
+            
         }
     } catch (error) {
         console.error('Error:', error);
     }
-    // console.log(tracks)
     return tracks
 };
 
