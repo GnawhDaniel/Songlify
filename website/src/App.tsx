b2e7e108812a1129dd3058db592afce3fffcce60
Daniel Hwang <danielhwang02@gmail.com>
2024-01-21 00:17:07 -0600

    deleted unused imports.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Search from "@components/Search"
 import SongsPage from '@components/SongsPage';
-import Test from '@components/Test';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -14,7 +13,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Search/>}></Route>
             <Route path="/playlist/:id" element={<SongsPage/>}></Route>
-            <Route path="/test2" element={<Test/>}></Route>
           </Routes>
         </Router>
     </>
